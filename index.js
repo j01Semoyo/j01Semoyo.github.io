43eb141aac75b857441835a737addfd83ffcbdcc
Semoyo j <j01.semoyo@gmail.com>
2020-08-23 12:18:20 +0000

    first init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const path = "data.json";
+
+var data = {
+  data:[]
+}
+
+fs.writeFileSync(path,JSON.stringify(data,null,4),"utf8",(e)=>{
+  console.log(e)
+})
+
+
+//  fs.readFile(path,"utf8",(e,data)=>{
+//   console.log(JSON.parse(data).data);
+  
+// })
+
